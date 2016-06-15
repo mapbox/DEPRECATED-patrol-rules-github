6386f2af1518e1a08177a4b9ea3734e4e3bc188f
arunasank <aruna@mapbox.com>
2016-06-14 21:37:53 -0400

    rule to detect force push

---
+++ rules/forcePush.js
@@ -0,0 +1,36 @@
+var message = require('lambda-cfn').message;
+var splitOnComma = require('lambda-cfn').splitOnComma;
+var util = require('util');
+
+module.exports.config = {
+  name: 'forcePush',
+  sourcePath: 'rules/forcePush.js',
+  gatewayRule: {
+    method: 'POST',
+    apiKey: false
+  }
+};
+
+module.exports.fn = function(event,callback) {
+  if (event.zen != undefined) {
+    var ping = 'GitHub ping event received';
+    console.log(ping);
+    return callback(null, ping);
+  } else {
+    if (event.forced && event.repository && event.pusher) {
+      var notif = {
+        subject: (util.format('Force push on %s by %s', event.repository.name, event.pusher.name)).substring(0,100),
+        summary: util.format('%s force pushed to %s', event.pusher.name, event.repository.name),
+        event: event
+      };
+      message(notif, function(err,result) {
+        console.log(JSON.stringify(notif));
+        return callback(err,result);
+        });
+    } else {
+      var badmsg = 'Error: unknown payload received';
+      console.log(badmsg);
+      return callback(badmsg);
+    }
+  }
+};
