f2f35aa3c7442026eb03261650851997f67c9a6f
arunasank <aruna@mapbox.com>
2016-06-21 12:32:52 +0530

    Lint and refactor

--- rules/forcePush.js
+++ rules/forcePush.js
@@ -11,29 +11,30 @@ module.exports.config = {
   }
 };
 
-module.exports.fn = function(event,callback) {
+module.exports.forcePush = function(event,callback) {
   if (event.zen != undefined) {
-    console.log('event zen');
-    var ping = 'GitHub ping event received';
-    console.log(ping);
-    return callback(null, ping);
+      var ping = 'GitHub ping event received';
+      return callback(null, ping);
   } else {
-    console.log('event okay');
-    if (event.forced && event.repository && event.pusher && event.ref.indexOf(event.repository.master_branch)) {
-      var notif = {
-        subject: (util.format('Force push on %s by %s', event.repository.name, event.pusher.name)).substring(0,100),
-        summary: util.format('%s force pushed to %s', event.pusher.name, event.repository.name),
-        event: event
-      };
-      message(notif, function(err,result) {
-        if(err) console.log('error ', err);
-        console.log(JSON.stringify(notif));
-        return callback(err,result);
-        });
-    } else {
-      var badmsg = 'Error: unknown payload received';
-      console.log(badmsg);
-      return callback(badmsg);
+      if (event.forced && event.repository && event.pusher && event.ref.indexOf(event.repository.master_branch) > -1) {
+          var notification = {
+              subject: (util.format('Force push on the %s branch of %s by %s', event.repository.name, event.repository.master_branch, event.pusher.name)).substring(0,100),
+              summary: util.format('%s force pushed on %s branch to %s', event.pusher.name, event.repository.master_branch, event.repository.name),
+              event: event
+          };
+          message(notification, function(err,result) {
+              if(err) console.log('error ', err);
+              return callback(err,result);
+          });
+      } else {
+            if(event.forced && event.repository && event.pusher && event.ref.indexOf(event.repository.master_branch) === -1) {
+                return callback(null,'Force push to a branch that is not master');
+            }
+            else {
+                var badmsg = 'Error: unknown payload received';
+                console.log(badmsg);
+                return callback(badmsg);
+            }
+        }
     }
-  }
 };
