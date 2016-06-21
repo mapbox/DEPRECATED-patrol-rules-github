79bec9dc700db4683a06b645b491bed7fba8df1a
arunasank <aruna@mapbox.com>
2016-06-21 12:33:13 +0530

    Add more tests

--- test/rules/forcePush.test.js
+++ test/rules/forcePush.test.js
@@ -1,9 +1,11 @@
 var test = require('tape');
 var rule = require('../../rules/forcePush.js');
 var testData = {
+  ref: "refs/heads/master",
   forced: true,
   repository: {
-    name: "public-repo"
+    name: "public-repo",
+    master_branch: "master"
     },
   pusher: {
     name: "arunasank",
@@ -16,16 +18,23 @@ var githubPing = {
 };
 
 test('GitHub ping', function(t) {
-  rule.fn(githubPing, function(err, message) {
+  rule.forcePush(githubPing, function(err, message) {
     t.equal(message,'GitHub ping event received');
     t.end();
   });
 });
 
 test('Well formed webhook payload', function(t) {
-  rule.fn(testData, function(err,message) {
-    // t.equal(message.event, testData,'events matched');
-    console.log('######## msg ',message);
+  rule.forcePush(testData, function(err,message) {
+    t.equal(message.event, testData,'events matched');
+    t.end();
+  });
+});
+
+test('Event for push to a branch that is not master', function(t) {
+  testData.ref = "refs/heads/branch-on-a-tree";
+  rule.forcePush(testData, function(err,message) {
+    t.equal(message, 'Force push to a branch that is not master','message matches');
     t.end();
   });
 });
