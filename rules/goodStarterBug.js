5b6cb526d96c36f9f659d475153d6e6128d8f017
arunasank <aruna@mapbox.com>
2016-07-29 13:36:13 +0530

    lint a little

--- rules/goodStarterBug.js
+++ rules/goodStarterBug.js
@@ -12,30 +12,26 @@ module.exports.config = {
 };
 
 
-module.exports.goodStarterBug = function(event,callback) {
+module.exports.goodStarterBug = function (event, callback) {
  if (event.zen != undefined) {
       var ping = 'GitHub ping event received';
       return callback(null, ping);
- } else{
- 	if (event.labels && event.action === 'labeled'){
- 		event.labels.forEach(function(item,index){
- 			if (item.name === 'good-starter-bug') {
- 				console.log('good-starter-bug label present');
- 				var notification = {
-                 subject: (util.format('`good-starter-bug`: In %s by %s', event.repository.name, event.issue.user.login)).substring(0,100),
-                 summary: util.format('%s applied `good-starter-bug` label on %s', event.issue.user.login, event.issue.html_url),
-                 event: event
-             };
-            message(notification, function(err,result) {
-              if(err) console.log('error ', err);
-              return callback(err,result);
-            });
-
- 			}
-
- 		});
- 		
- 	}
-
+ } else {
+    if (event.action === 'labeled') {
+        event.labels.forEach(function (item, index) {
+            if (item.name === 'good-starter-bug') {
+                console.log('good-starter-bug label present');
+                var notification = {
+                    subject: (util.format('`good-starter-bug`: In %s by %s', event.repository.name, event.issue.user.login)).substring(0,100),
+                    summary: util.format('%s applied `good-starter-bug` label on %s', event.issue.user.login, event.issue.html_url),
+                    event: event
+                };
+                message(notification, function(err, result) {
+                    if(err) console.log('error ', err);
+                    return callback(err, result);
+                });
+            }
+        });
+    }
   }
 };
