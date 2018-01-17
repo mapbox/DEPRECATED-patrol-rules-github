83579c375cd620b97da59b00c89fd5068ad0776e
Edgar Marca <matiskay@gmail.com>
2018-01-17 13:25:53 -0500

    Add madePublicDispatch rule

--- madePublicDispatch/function.js
+++ madePublicDispatch/function.js
@@ -1,15 +1,55 @@
-const lambdaCfn = require('@mapbox/lambda-cfn');
-const github = require('../lib/madePublic.js');
+const AWS = require('aws-sdk');
+const github = require('../lib/madePublic/event.js');
 
 function notify(event, callback) {
-  let message = {
-    subject: (`Private repository ${event.repository.name} made public by ${event.sender.login}`).substring(0, 100),
-    summary: `The private repository ${event.repository.name} was made by public by github user ${event.sender.login} at ${event.repository.updated_at}\nRepo: ${event.repository.html_url}\nUser: ${event.sender.html_url}`,
-    event: event
+  let repoName = event.repository.name;
+  let githubUser = event.repository.sender.login;
+
+  const sns = new AWS.SNS();
+  const message = {
+    type: 'self-service',
+    retrigger: false,
+    users: [
+      {
+        github: githubUser
+      }
+    ],
+    body: {
+      github: {
+        title: `${githubUser} made a ${repoName} repo public`,
+        body: `Hey there @${repoName} 👋 ! It looks like you made a ${repoName} repo public 
+
+Check the following list to ensure that the repo is not leaking sensitive information:
+
+* Check README and documentation for any Company-isms with sensitive information
+* Check code for sensitive information (e.g. hard coded credentials, comments with sensitive info)
+* Check tests for any fixtures using live data
+* Check tests for any testing or debug URLs
+* Check code, tests, and documentation on other branches
+* Use https://rtyley.github.io/bfg-repo-cleaner/ to clean your sensitive data
+
+Consider notify the security team in case of any information leak.
+        `,
+      },
+      slack: {
+        message: `Hey there! :wave: It looks like you made the ${repoName} repo public.`,
+        prompt: 'Is this a public repository?',
+        actions: {
+          yes: 'Yes',
+          'yes_response': 'Great, please comment on the /dispatch-alerts Github issue to explain why this is a public repo.',
+          no: 'No, this is a private repo.',
+          'no_response': 'Oh no! We paged the security L1 on call and they will assist you soon.',
+        },
+      },
+    }
   };
-  lambdaCfn.message(message, (err, result) => {
-    console.log(JSON.stringify(message));
-    return callback(err, result);
+  const params = {
+    Message: JSON.stringify(message),
+    TopicArn: dispatchSnsArn
+  };
+  sns.publish(params, function(err,data) {
+    if (err) return callback(err);
+    return callback(null, data);
   });
 }
 
--- madePublicDispatch/function.template.js
+++ madePublicDispatch/function.template.js
@@ -1,3 +1,34 @@
-const madePublicCfn = require('../lib/madePublicCfn');
+const lambdaCfn = require('@mapbox/lambda-cfn');
+const madePublicEventSources = require('../lib/madePublic/cfnEventSources');
 
-module.exports = madePublicCfn.cfn;
+let cfnTemplate = madePublicEventSources.eventsources;
+
+cfnTemplate.name = 'madePublicDispatch';
+
+cfnTemplate.parameters = {
+  dispatchSnsArn: {
+    Type: 'String',
+    Description: 'Dispatch SNS ARN'
+  }
+};
+
+cfnTemplate.statements = [
+  {
+    Effect: 'Allow',
+    Action: [
+      'kms:Decrypt'
+    ],
+    Resource: {
+      'Fn::ImportValue':'cloudformation-kms-production'
+    }
+  },
+  {
+    Effect: 'Allow',
+    Action: 'sns:Publish',
+    Resource: {
+      Ref: 'dispatchSnsArn'
+    }
+  }
+];
+
+module.exports = lambdaCfn.build(cfnTemplate);
