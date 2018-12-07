1a9e738ac53df337003e9df10a131bd84e5497fd
Zachariah Mully <mully@mapbox.com>
2018-12-07 15:59:59 -0500

    update to Artifacts

--- madePublic/function.template.js
+++ madePublic/function.template.js
@@ -1,6 +1,7 @@
 const lambdaCfn = require('@mapbox/lambda-cfn');
+const cf = require('@mapbox/cloudfriend');
 
-let cfnTemplate = {
+const lambdaTemplate = lambdaCfn.build({
   name: 'madePublic',
   eventSources: {
     webhook: {
@@ -8,6 +9,14 @@ let cfnTemplate = {
       apiKey: false
     }
   }
-};
+});
 
-module.exports = lambdaCfn.build(cfnTemplate);
+delete lambdaTemplate.Parameters.CodeS3Bucket;
+delete lambdaTemplate.Parameters.CodeS3Prefix;
+delete lambdaTemplate.Resources.madePublic.Properties.Environment.Variables.CodeS3Bucket;
+delete lambdaTemplate.Resources.madePublic.Properties.Environment.Variables.CodeS3Prefix;
+
+lambdaTemplate.Resources.madePublic.Properties.Code.S3Bucket = cf.join('-', ['utility', cf.accountId, cf.region]);
+lambdaTemplate.Resources.madePublic.Properties.Code.S3Key = cf.join('', ['bundles/patrol-rules-github/', cf.ref('GitSha'), '.zip']);
+
+module.exports = lambdaTemplate;
